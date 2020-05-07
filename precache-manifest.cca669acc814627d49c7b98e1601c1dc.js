c260e12d3c52964cb2236448a69c062c992295d1
Wouter van den Heuvel <exo_00@hotmail.com>
2020-05-07 17:52:38 +0200

    Updates

---
+++ precache-manifest.cca669acc814627d49c7b98e1601c1dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f4b108d1d79d587716ba1756689428",
+    "url": "/louwman/index.html"
+  },
+  {
+    "revision": "79737d53989243b865fb",
+    "url": "/louwman/static/css/main.fbba1791.chunk.css"
+  },
+  {
+    "revision": "da0af87756a5afc2b407",
+    "url": "/louwman/static/js/2.1145baad.chunk.js"
+  },
+  {
+    "revision": "7efcc7dfa094cdaa895a206530158a18",
+    "url": "/louwman/static/js/2.1145baad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79737d53989243b865fb",
+    "url": "/louwman/static/js/main.676ebb40.chunk.js"
+  },
+  {
+    "revision": "06586a4953a537395273",
+    "url": "/louwman/static/js/runtime-main.79c9a56e.js"
+  },
+  {
+    "revision": "81786b3c0eb5c4e32f4a549579df8997",
+    "url": "/louwman/static/media/check.81786b3c.svg"
+  }
+]);
